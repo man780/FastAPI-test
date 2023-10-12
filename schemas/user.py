98eb9d99526d0fb3c8945cddcc6636b526752903
m.tatybaev <m.tatybaev@qiwi.uz>
2023-10-13 02:43:42 +0500

    feet: validators in schema user

--- schemas/user.py
+++ schemas/user.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 
 class UserBase(BaseModel):
-    username: str
-    email: str
-    full_name: str
+    username: str = Field(..., min_length=1, max_length=50)
+    email: str = Field(..., min_length=4, max_length=50)
+    full_name: str = Field(..., min_length=5, max_length=50)
+
+    @validator("username", "email", "full_name")
+    def validate_no_sql_injection(cls, value):
+        if "delete from" in value:
+            raise ValueError("Our terms strictly prohobit SQLInjection Attacks")
+        return value
 
 
 class UserCreate(UserBase):
