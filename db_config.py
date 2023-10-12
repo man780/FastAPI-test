88c1988540cfd95fd30b1445c897125a9798fffd
m.tatybaev <m.tatybaev@qiwi.uz>
2023-10-13 02:01:30 +0500

    feet: db_config

--- db_config.py
+++ db_config.py
@@ -10,3 +10,16 @@ engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
+
+
+async def get_db():
+    async_db = async_session()
+    try:
+        yield async_db
+    finally:
+        await async_db.close()
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
