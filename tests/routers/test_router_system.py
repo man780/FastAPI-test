0afe4d0be204dd3cf11ac5cff456e73ab6153049
m.tatybaev <m.tatybaev@qiwi.uz>
2023-10-13 15:05:22 +0500

    feet: test system

--- tests/routers/test_router_system.py
+++ tests/routers/test_router_system.py
@@ -1,4 +1,14 @@
 def test_health_check(test_app):
     response = test_app.get("/health/check")
     assert response.status_code == 200
-    # assert response.json() == {"ping": "pong!"}
+    response_data = response.json()
+    assert response_data["message"] == "Success"
+    assert response_data["status"] == True
+
+
+# def test_health_check_db(test_app):
+#     response = test_app.get("/health/check/db")
+#     assert response.status_code == 200
+#     response_data = response.json()
+#     assert response_data["message"] == "Success"
+#     assert response_data["status"] == True
