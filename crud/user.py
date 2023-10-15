f8d8cba84c522c4220cf8ac6304eefbfd759b9c4
m.tatybaev <m.tatybaev@qiwi.uz>
2023-10-16 02:36:06 +0500

    refactor: crud pack with pylint

--- crud/user.py
+++ crud/user.py
@@ -1,3 +1,4 @@
+"""User CRUD operations"""
 import logging
 from logging.handlers import RotatingFileHandler
 from typing import Any, Sequence
@@ -18,6 +19,13 @@ logger = logging.getLogger(__name__)
 
 
 async def create_user(db: AsyncSession, user: UserCreate) -> Row:
+    """
+    Create user
+
+    :param db:
+    :param user:
+    :return: user Row
+    """
     try:
         user = UserModel(**user.model_dump())
         db.add(user)
@@ -25,11 +33,18 @@ async def create_user(db: AsyncSession, user: UserCreate) -> Row:
         await db.refresh(user)
         return user
     except Exception as ex:
-        logger.error(f"Error on user create: {ex}")
-        raise HTTPException(status_code=400, detail=dict(error_message=f"{ex}"))
+        logger.error("Error on user create: %s", ex)
+        raise HTTPException(status_code=400, detail={"error_message": f"{ex}"}) from ex
 
 
 async def get_user(db: AsyncSession, user_id: int) -> Row[Any] | None:
+    """
+    Get user by id
+
+    :param db:
+    :param user_id:
+    :return: user Row
+    """
     result = await db.execute(
         UserModel.__table__.select().where(UserModel.id == user_id)
     )
@@ -38,6 +53,14 @@ async def get_user(db: AsyncSession, user_id: int) -> Row[Any] | None:
 
 
 async def update_user(db: AsyncSession, user_id: int, user_data: UserUpdate):
+    """
+    Update user by id
+
+    :param db:
+    :param user_id:
+    :param user_data:
+    :return: user Row
+    """
     try:
         user_dict = user_data.model_dump()
         update_stmt = (
@@ -52,25 +75,43 @@ async def update_user(db: AsyncSession, user_id: int, user_data: UserUpdate):
         user = await get_user(db=db, user_id=user_id)
         return user
     except Exception as ex:
-        raise HTTPException(status_code=400, detail=dict(error_message=f"{ex}"))
+        logger.error("Error on user update: %s", ex)
+        raise HTTPException(status_code=400, detail={"error_message": f"{ex}"}) from ex
 
 
 async def delete_user(db: AsyncSession, user_id: int) -> None:
+    """
+    Delete user by id
+
+    :param db:
+    :param user_id:
+    :return:
+    """
     try:
         deleted_stmt = delete(UserModel.__table__).where(UserModel.id == user_id)
 
         await db.execute(deleted_stmt)
         await db.commit()
     except Exception as ex:
-        raise HTTPException(status_code=400, detail=dict(error_message=f"{ex}"))
+        logger.error("Error on user delete: %s", ex)
+        raise HTTPException(status_code=400, detail={"error_message": f"{ex}"}) from ex
 
 
 async def list_users(
     db: AsyncSession, skip: int = 0, limit: int = 10
 ) -> Sequence[Row[Any]]:
+    """
+    Get users list
+
+    :param db:
+    :param skip:
+    :param limit:
+    :return:
+    """
     try:
         query = await db.execute(UserModel.__table__.select().offset(skip).limit(limit))
         users = query.fetchall()
         return users
     except Exception as ex:
-        raise HTTPException(status_code=400, detail=dict(error_message=f"{ex}"))
+        logger.error("Error on getting users list: %s", ex)
+        raise HTTPException(status_code=400, detail={"error_message": f"{ex}"}) from ex
